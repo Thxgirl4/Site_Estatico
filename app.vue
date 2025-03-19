1ed8719ac320683865bfc6a0c2f918bf42a18efa
Thxgirl4 <anajuliasilva3304@gmail.com>
2025-03-19 12:52:45 -0300

    teste com vue.js

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Pagina />
+  </div>
+</template>
+
+<script>
+import Pagina from './components/Pagina.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Pagina
+  }
+};
+</script>
+
+<style>
+/* Adicione estilos globais aqui, se necessário */
+</style>
---
+++ pagina.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      In finibus magna eros, ut hendrerit tortor aliquam eu.
+      Morbi libero augue, ornare ac mauris vel, lobortis malesuada justo.
+      Sed blandit est id tincidunt sollicitudin.
+      Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+      Donec blandit interdum nunc, in pharetra augue sodales sed.
+      Sed tellus lectus, consequat vel venenatis et, euismod at orci.
+      Aliquam faucibus ligula sed justo gravida imperdiet.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Pagina",
+};
+</script>
+
+<style scoped>
+/* Adicione estilos aqui, se necessário */
+</style>
